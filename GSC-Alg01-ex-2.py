f2884aa3466b0515e6435d9a03bb3b110fe0c3fe
GUILHERME SERAFIN DE CARVALHO <98965845+guiserafin@users.noreply.github.com>
2022-05-03 15:52:14 -0300

    Delete GSC-Alg01-ex-2.py

--- GSC-Alg01-ex-2.py
+++
@@ -1,9 +0,0 @@
-"""Saudação. Escreva um programa que pergunte o nome do(a) usuário(a) e em seguida exiba
-uma mensagem de “Olá" ao usuário, utilizando o nome fornecido."""
-
-import string
-
-
-nome = input("Escreva seu nome: ")
-
-print("Olá, " +nome)
