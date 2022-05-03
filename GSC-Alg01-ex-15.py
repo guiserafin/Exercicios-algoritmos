126115c9b3a12390b95a5a6404933bf5398897fc
GUILHERME SERAFIN DE CARVALHO <98965845+guiserafin@users.noreply.github.com>
2022-05-03 15:52:25 -0300

    Delete GSC-Alg01-ex-15.py

--- GSC-Alg01-ex-15.py
+++
@@ -1,9 +0,0 @@
-"""Escreva um programa Python que obtenha do usuário os valores de l e n, e então calcule e
-exiba a área do polígono regular correspondente."""
-import math
-n = int(input("Insira o número de lados do polígono: "))
-l = float(input("Insira o comprimento de um dos lados do polígono: "))
-
-area = (n*(l**2))/ 4* math.tan(math.pi/n)
-
-print("A área do polígono é de %.2f unidades de área"%area)
