770c05aa6fa6944c755493ea378cb3ed2ac23e58
GUILHERME SERAFIN DE CARVALHO <98965845+guiserafin@users.noreply.github.com>
2022-05-04 10:33:22 -0300

    Update GSC-Alg04-ex-12.py

--- Lista 4/GSC-Alg04-ex-12.py
+++ Lista 4/GSC-Alg04-ex-12.py
@@ -3,9 +3,12 @@ mostrando os produtos de todos os inteiros de 1 vezes 1 até 10 vezes 10. Sua ta
 incluir uma linha de cabeçalho com números de 1 a 10, e também uma coluna com os
 mesmos números. A saída esperada do programa deve ser semelhante ao mostrado abaixo:"""
 
-for i in range(1, 11):
-    for j in range(1, 11):
-        multiplicação = i * j
-        print(multiplicação, end=" ")
-        j = j + 1
-    i = i + 1
+print("\t", end =" ")
+for coluna in range (1, 11)
+    print(coluna, "\t", end=" ")
+print()
+for linha in range (1,11):
+    print(linha, "\t", end =" ")
+    for coluna in range (1,11):
+        print(linha*coluna, "\t", end =" ")
+    print()
